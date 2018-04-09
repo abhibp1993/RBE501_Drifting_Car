4b9d602529866f93f86ad987e22ffadfd8224b2d
Radian A G <radiang91@gmail.com>
2018-04-09 02:47:06 -0400

    cleanup

--- matlab/linear_regression.m
+++ matlab/linear_regression.m
@@ -24,59 +24,6 @@ minx = min(gin);
 [yaw_init] = regress(yaw_init,gamma,yaw,minx,maxx,2,0,i);
 [yaw_init] = regress(yaw_init,gamma,yaw,minx,maxx,3,0,i);
 
-% %%  Red Stuff Regression
-%  [idx] = find(vy(2,:,20)>0);
-%  
-% for i =1:length(idx)
-%     if idx(i)<max
-%     x(end+1) = gamma(idx(i));
-%     y(end+1)=vy(2,idx(i),20);
-%     end
-% end
-% 
-% %% Linear Regression
-% X = [ones(length(x),1) x'];
-% b = X\y';
-% b(2)=1*b(2);
-% 
-% %% Interpolate
-% r_int = gamma(min:end);
-% X2 = [ones(length(r_int),1), r_int'];
-% y_int = X2*b;
-% %plot(r_int,y_int,'--');
-% %hold on
-% 
-% new_init(2,min:end)=y_int;
-% 
-% 
-% %% Green Stuff Regression
-% x=[];
-% y=[];
-% idx=[];
-% 
-% [idx] = find(vy(3,:,20)<0);
-%  
-% for i =1:length(idx)
-%     if idx(i)>min
-%     x(end+1) = gamma(idx(i));
-%     y(end+1)=vy(3,idx(i),20);
-%     end
-% end
-% %% Linear Regression
-% X = [ones(length(x),1) x'];
-% b = X\y';
-% b(2)=1*b(2);
-% 
-% %% Interpolate
-% r_int = gamma(1:max);
-% X2 = [ones(length(r_int),1), r_int'];
-% y_int = X2*b;
-% 
-% %plot(r_int,y_int,'--');
-% 
-% new_init(3,min:end)=y_int;
-% 
-
 
 
     function [new_init] = regress(new_init,gamma,vy,min,max,color,VorY,i)
