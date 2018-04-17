bf5fdf41c7ff81d3f0405523ad5c6910ee4dddcf
justpuckette <justpuckette@users.noreply.github.com>
2018-04-16 20:15:25 -0400

    Add files via upload
    Updated Sample Cases

--- Simulink/Run_VehicleDynamics3D.m
+++ Simulink/Run_VehicleDynamics3D.m
@@ -27,17 +27,24 @@ open('VehicleDynamics3D_controller.slx');
 %% Run Simulink Model
 
 % simcase = 'drive_straight';
-simcase = 'PI_sample';
-
+% simcase = 'shallow_turn';
+simcase = 'PID_sample';
+% simcase = 'throttle_control';
+ste_gain = 1;
+thl_gain = 0;
 
 switch simcase
     
     case 'drive_straight'
-        stoptime        =   5;  % simulation end time,                [s]
-        switch_control  = 100;  % time when PI receives setpoint,     [s]
-        switch_steering = 100;  % switch from fixed steering to PI,   [s]
-        steering_step_t = 100;  % steering step time to induce drift, [s]
+        stoptime        =  15;  % simulation end time,                  [s]
+        switch_steering_control  = 100;  
+                                % time when steer PI receives setpoint, [s]
+        switch_steering = 100;  % switch from fixed steering to PI,     [s]
+        steering_step_t = 100;  % steering step time to induce drift,   [s]
         steering_step   =   0;  % steering angle to induce drift
+        switch_throttle = 100;  % switch from fixed throttle to PI,     [s]
+        switch_speed_control  = 100;  
+                                % time when speed PI receives setpoint, [s]
         max_speed       =  20;  % kph 
         lat_xmin        = -10;  % min x value, position plot, [m]
         lat_ymin        = -10;  % min y value, position plot, [m]
@@ -45,18 +52,57 @@ switch simcase
         lat_ymax        =  200; % max y value, position plot, [m]
         latv_setpoint   =    0; % lateral velocity setpoint,  [kph]
         
-    case 'PI_sample'
-        stoptime        =   25;  % simulation end time,                [s]
-        switch_control  =    6;  % time when PI receives setpoint,     [s]
-        switch_steering =    6;  % switch from fixed steering to PI,   [s]
-        steering_step_t =  0.1;  % steering step time to induce drift, [s]
+    case 'shallow_turn'
+        stoptime        =  15;  % simulation end time,                  [s]
+        switch_steering_control  = 100;  
+                                % time when steer PI receives setpoint, [s]
+        switch_steering = 100;  % switch from fixed steering to PI,     [s]
+        steering_step_t =   5;  % steering step time to induce drift,   [s]
+        steering_step   =  -1;  % steering angle to induce drift
+        switch_throttle = 100;  % switch from fixed throttle to PI,     [s]
+        switch_speed_control  = 100;  
+                                % time when speed PI receives setpoint, [s]
+        max_speed       =  15;  % kph 
+        lat_xmin        = -10;  % min x value, position plot, [m]
+        lat_ymin        = -10;  % min y value, position plot, [m]
+        lat_xmax        =  200; % max x value, position plot, [m]
+        lat_ymax        =  200; % max y value, position plot, [m]
+        latv_setpoint   =    0; % lateral velocity setpoint,  [kph]
+    case 'PID_sample'
+        stoptime        =   25;  % simulation end time,                 [s]
+        switch_steering_control  = 3.4;  
+                                % time when PI receives setpoint,       [s]
+        switch_steering =   3.5;  % switch from fixed steering to PI,    [s]
+%         switch_steering =   100;  % switch from fixed steering to PI,    [s]
+        steering_step_t =  0.01;  % steering step time to induce drift,  [s]
         steering_step   =  -30;  % steering angle to induce drift
-        max_speed       =   15;  % kph 
+        switch_throttle =  90;  % switch from fixed throttle to PI,     [s]
+        switch_speed_control  = 100;  
+                                % time when speed PI receives setpoint, [s]    
+        max_speed       =   70;  % kph 
         lat_xmin        = -100;  % min x value, position plot, [m]
         lat_ymin        =  -10;  % min y value, position plot, [m]
         lat_xmax        =   25; % max x value, position plot,  [m]
         lat_ymax        =   50; % max y value, position plot,  [m]
-        latv_setpoint   =  -20; % lateral velocity setpoint,   [kph]
+        latv_setpoint   =  -12; % lateral velocity setpoint,   [kph]
+        
+    case 'throttle_control'
+        % still workong on throttle control
+%         stoptime        =   25;  % simulation end time,                [s]
+%         switch_steering_control  = 100;  
+%                                  % time when PI receives setpoint,     [s]
+%         switch_steering =  100;  % switch from fixed steering to PI,   [s]
+%         steering_step_t =  0.01;  % steering step time to induce drift, [s]
+%         steering_step   =  -13;  % steering angle to induce drift
+%         switch_throttle =    6;  % switch from fixed throttle to PI,     [s]
+%         switch_speed_control  = 6;  
+%                                 % time when speed PI receives setpoint, [s]                
+%         max_speed       =   60;  % kph 
+%         lat_xmin        = -100;  % min x value, position plot, [m]
+%         lat_ymin        =  -10;  % min y value, position plot, [m]
+%         lat_xmax        =   25; % max x value, position plot,  [m]
+%         lat_ymax        =   50; % max y value, position plot,  [m]
+%         latv_setpoint   =   -6; % lateral velocity setpoint,   [kph]
         
 end
 
@@ -67,18 +113,29 @@ Vy_NR = simout.Vy_NR.Data;  % car body velocity wrt non-rotating follower
 tout  = simout.Vy_NR.Time;  % time vector
 ster  = simout.SteeringAngle_deg.Data;
                             % steering angle
+spd   = simout.Speed_input.Data;       
 %% Plot Results
 
 figure;
-subplot(2,1,1)
+subplot(3,1,1)
 plot(tout,Vx_NR)
 grid on
 ylabel('[kph]')
+ylim([-30 30])
 title('Car Lateral Velocity')
 
-subplot(2,1,2)
+subplot(3,1,2)
 plot(tout,ster)
 grid on
 ylabel('[degrees]')
 title('Car Steering Angle')
+
+% wheel outer radius is 30cm
+wheel_rpm = spd/0.3/2/pi;
+
+subplot(3,1,3)
+plot(tout,wheel_rpm)
+grid on
+ylabel('[rpm]')
+title('Rear Wheel Speed')
 
