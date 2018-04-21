16299c86ce51c70aba5886187edccc9eeb68d2ba
justpuckette <justpuckette@users.noreply.github.com>
2018-04-20 22:20:23 -0400

    Added default cases and PID gains
    For throttle and steering control cases + a combined steering & control case

--- Simulink/Run_VehicleDynamics3D.m
+++ Simulink/Run_VehicleDynamics3D.m
@@ -29,18 +29,22 @@ open('VehicleDynamics3D_controller.slx');
 %% Pick a Sample Case, for Monte Carlo use PID
 % simcase = 'drive_straight';
 % simcase = 'shallow_turn';
-% simcase = 'PID_sample';
-simcase = 'throttle_control';
+% simcase = 'steering_control';
+% simcase = 'throttle_control';
+simcase = 'combined_control';
 
 %% Choose Steering Control or Throttle Control
 ste_gain = 1;
 thl_gain = 1;
 
+%% Open loop or closed loop
+openloop = 0;
+
 % Number of monte carlo cases
 n_cases = 1;
 
 % preallocate output vector
-MCout = zeros(7,n_cases)*NaN;
+MCout = zeros(10,n_cases)*NaN;
 
 for n = 1:n_cases
 tic
@@ -55,7 +59,7 @@ P = 1.2095; % recommended value
 % a = 0.50;
 % b = 0.6;
 % I = (b-a).*rand(1) + a;
-I =0.55564; % recommended value
+I = 0.55564; % recommended value
 % 
 % a = 0.125;
 % b = 0.25;
@@ -63,22 +67,47 @@ I =0.55564; % recommended value
 D = 0.1341; % recommended value
 
 %% Random PID for Monte Carlo, throttle
-a = 2.10;
-b = 2.25;
-Pt = (b-a).*rand(1) + a;
-Pt = 2.1389; % recommended value
+% a = 1.9;
+% b = 3.5;
+% Pt = (b-a).*rand(1) + a;
+% % Pt = 2.1389; % recommended value
+% % Pt = 3;
+% % Pt = 2.5*Pt; 
+% 
+% a = 0.6;
+% b = 0.1;
+% It = (b-a).*rand(1) + a;
+% % It = 0.7671; % recommended value
+% % It = 1.4;
+% % It = 2.5*It;
+% 
+% a = 0.20;
+% b = 0.70;
+% Dt = (b-a).*rand(1) + a;
+% % Dt = 0.65979; % recommended value
+% % Dt = 0.34;
+% % Dt = 2.5*Dt;
 
-a = 0.76;
-b = 0.9;
-It = (b-a).*rand(1) + a;
-It = 0.7671; % recommended value
-
-a = 0.55;
-b = 0.70;
-Dt = (b-a).*rand(1) + a;
-Dt = 0.65979; % recommended value
 
 
+%% Random PID for Monte Carlo, throttle
+% a = 2.10;
+% b = 2.25;
+% Pt = (b-a).*rand(1) + a;
+Pt = 2.1389*2.5; % recommended value
+% Pt = 2.5*Pt; 
+% 
+% a = 0.76;
+% b = 0.9;
+% It = (b-a).*rand(1) + a;
+It = 0.7671*2.5; % recommended value
+% It = 2.5*It;
+% 
+% a = 0.55;
+% b = 0.70;
+% Dt = (b-a).*rand(1) + a;
+Dt = 0.65979*2.5; % recommended value
+% Dt = 2.5*Dt;
 
 switch simcase
     
@@ -115,7 +144,7 @@ switch simcase
         lat_xmax        =  200; % max x value, position plot, [m]
         lat_ymax        =  200; % max y value, position plot, [m]
         latv_setpoint   =    0; % lateral velocity setpoint,  [kph]
-    case 'PID_sample'
+    case 'steering_control'
         stoptime        =   25;  % simulation end time,                 [s]
         switch_steering_control  = 3.58;  
                                 % time when PI receives setpoint,       [s]
@@ -138,12 +167,12 @@ switch simcase
         stoptime        =   25;  % simulation end time,                [s]
         switch_steering_control  = 3.58;  
                                 % time when PI receives setpoint,       [s]
-        switch_steering =   3.58;  % switch from fixed steering to PI,    [s]
+%         switch_steering =   3.58;  % switch from fixed steering to PI,    [s]
         switch_steering =  100;  % switch from fixed steering to PI,   [s]
         steering_step_t =  0.01;  % steering step time to induce drift, [s]
         steering_step   =  -30;  % steering angle to induce drift
-        switch_throttle =    3.28;  % switch from fixed throttle to PI,     [s]
-        switch_speed_control  = 3.28;  
+        switch_throttle =    3.58;  % switch from fixed throttle to PI,     [s]
+        switch_speed_control  = 3.58;  
                                 % time when speed PI receives setpoint, [s]      
 %         switch_speed_control  = 100;  
                                 % time when speed PI receives setpoint, [s]                           
@@ -154,6 +183,34 @@ switch simcase
         lat_ymax        =   50; % max y value, position plot,  [m]
         latv_setpoint   =   -12.28; % lateral velocity setpoint,   [kph]
         
+    case 'combined_control' 
+%         still workong on throttle control
+        stoptime        =   25;  % simulation end time,                [s]
+        switch_steering_control  = 3.58;  
+                                % time when PI receives setpoint,       [s]
+        switch_steering =   3.58;  % switch from fixed steering to PI,    [s]
+%         switch_steering =  100;  % switch from fixed steering to PI,   [s]
+        steering_step_t =  0.01;  % steering step time to induce drift, [s]
+        steering_step   =  -30;  % steering angle to induce drift
+        switch_throttle =    3.58;  % switch from fixed throttle to PI,     [s]
+        switch_speed_control  = 3.58;  
+                                % time when speed PI receives setpoint, [s]      
+%         switch_speed_control  = 100;  
+                                % time when speed PI receives setpoint, [s]                           
+        max_speed       =   80;  % kph 
+        lat_xmin        = -100;  % min x value, position plot, [m]
+        lat_ymin        =  -10;  % min y value, position plot, [m]
+        lat_xmax        =   25; % max x value, position plot,  [m]
+        lat_ymax        =   50; % max y value, position plot,  [m]
+        latv_setpoint   =   -12.28; % lateral velocity setpoint,   [kph]
+        
+       
+        
+end
+
+if openloop == 1
+    switch_throttle = 100;
+    switch_steering = 100;
 end
 
 simout = sim('VehicleDynamics3D_controller.slx');
@@ -199,24 +256,29 @@ MCout(1,n) = steering_step;
 MCout(2,n) = max_speed;
 MCout(3,n) = mean(Vx_NR(501:end));
 MCout(4,n) = S.normr;
-MCout(5,n) = P;
-MCout(6,n) = I;
-MCout(7,n) = D;
+MCout(5,n)  = P;
+MCout(6,n)  = I;
+MCout(7,n)  = D;
+MCout(8,n)  = Pt;
+MCout(9,n)  = It;
+MCout(10,n) = Dt;
+
 
 
 
 
 toPPT(gcf); % By default a new slide is created when adding a figure
-toPPT('setTitle',['P: ' num2str(P) ' I: ' num2str(I) ' D: ' num2str(D) ' normr: ' num2str(S.normr)]);
+toPPT('setTitle',['P: ' num2str(P) ' ' num2str(Pt) ' I: ' num2str(I) ' ' num2str(It) ' D: ' num2str(D) ' ' num2str(Dt) ' normr: ' num2str(S.normr)]);
 toc
 
-% close gcf
+close gcf
 
 end
 
 %% if desired, uncomment to save results to a randomly named .mat file
 % uniquename = ['MCout' randseq(3) '.mat'];
 % save(uniquename,'MCout')
+save('MCout_combined_control','MCout','simout')
 % 
 % 
 % %% if desired, uncomment to plot results
