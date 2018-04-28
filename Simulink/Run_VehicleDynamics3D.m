2e6e346fd55b0fd4873c227a3dc96d624bdbce71
justpuckette <justpuckette@users.noreply.github.com>
2018-04-28 17:09:09 -0400

    Updated the XY Plot
    Added better axis ranges to the XY Plot for the Throttle Control, Steering Control, and Combined Control Cases.  Increased the XY plot sample rate for smoother plot

--- Simulink/Run_VehicleDynamics3D.m
+++ Simulink/Run_VehicleDynamics3D.m
@@ -29,9 +29,9 @@ open('VehicleDynamics3D_controller.slx');
 %% Pick a Sample Case, for Monte Carlo use PID
 % simcase = 'drive_straight';
 % simcase = 'shallow_turn';
-% simcase = 'steering_control';
+simcase = 'steering_control';
 % simcase = 'throttle_control';
-simcase = 'combined_control';
+% simcase = 'combined_control';
 
 %% Choose Steering Control or Throttle Control
 ste_gain = 1;
@@ -149,17 +149,17 @@ switch simcase
         switch_steering_control  = 3.58;  
                                 % time when PI receives setpoint,       [s]
         switch_steering =   3.58;  % switch from fixed steering to PI,    [s]
-%         switch_steering =   100;  % switch from fixed steering to PI,    [s]
+%         switch_steering =   100;  % sswitch from fixed steering to PI,    [s]
         steering_step_t =  0.01;  % steering step time to induce drift,  [s]
         steering_step   =  -30;  % steering angle to induce drift
         switch_throttle =  80;  % switch from fixed throttle to PI,     [s]
         switch_speed_control  = 100;  
                                 % time when speed PI receives setpoint, [s]    
         max_speed       =   80;  % kph 
-        lat_xmin        = -100;  % min x value, position plot, [m]
-        lat_ymin        =  -10;  % min y value, position plot, [m]
-        lat_xmax        =   25; % max x value, position plot,  [m]
-        lat_ymax        =   50; % max y value, position plot,  [m]
+        lat_xmin        = -22;  % min x value, position plot, [m]
+        lat_ymin        =  -1;  % min y value, position plot, [m]
+        lat_xmax        =   1; % max x value, position plot,  [m]
+        lat_ymax        =   22; % max y value, position plot,  [m]
         latv_setpoint   =  -12.28; % lateral velocity setpoint,   [kph]
         
     case 'throttle_control' 
@@ -170,18 +170,18 @@ switch simcase
 %         switch_steering =   3.58;  % switch from fixed steering to PI,    [s]
         switch_steering =  100;  % switch from fixed steering to PI,   [s]
         steering_step_t =  0.01;  % steering step time to induce drift, [s]
-        steering_step   =  -30;  % steering angle to induce drift
+        steering_step   =  -15;  % steering angle to induce drift
         switch_throttle =    3.58;  % switch from fixed throttle to PI,     [s]
         switch_speed_control  = 3.58;  
                                 % time when speed PI receives setpoint, [s]      
 %         switch_speed_control  = 100;  
                                 % time when speed PI receives setpoint, [s]                           
-        max_speed       =   80;  % kph 
-        lat_xmin        = -100;  % min x value, position plot, [m]
-        lat_ymin        =  -10;  % min y value, position plot, [m]
-        lat_xmax        =   25; % max x value, position plot,  [m]
-        lat_ymax        =   50; % max y value, position plot,  [m]
-        latv_setpoint   =   -12.28; % lateral velocity setpoint,   [kph]
+        max_speed       =   300;  % kph 
+        lat_xmin        = -26;  % min x value, position plot, [m]
+        lat_ymin        =  -1;  % min y value, position plot, [m]
+        lat_xmax        =   1; % max x value, position plot,  [m]
+        lat_ymax        =   26; % max y value, position plot,  [m]
+        latv_setpoint   =   -8; % lateral velocity setpoint,   [kph]
         
     case 'combined_control' 
 %         still workong on throttle control
@@ -198,10 +198,10 @@ switch simcase
 %         switch_speed_control  = 100;  
                                 % time when speed PI receives setpoint, [s]                           
         max_speed       =   80;  % kph 
-        lat_xmin        = -100;  % min x value, position plot, [m]
-        lat_ymin        =  -10;  % min y value, position plot, [m]
-        lat_xmax        =   25; % max x value, position plot,  [m]
-        lat_ymax        =   50; % max y value, position plot,  [m]
+        lat_xmin        = -16;  % min x value, position plot, [m]
+        lat_ymin        =  -1;  % min y value, position plot, [m]
+        lat_xmax        =   1; % max x value, position plot,  [m]
+        lat_ymax        =   16; % max y value, position plot,  [m]
         latv_setpoint   =   -12.28; % lateral velocity setpoint,   [kph]
         
        
@@ -223,70 +223,70 @@ ster  = simout.SteeringAngle_deg.Data;
 spd   = simout.Speed_input.Data;       
 %% Plot Results
 
-figure;
-subplot(3,1,1)
-hold on
-plot(tout,Vx_NR,'m:')
-grid on
-ylabel('[kph]')
-ylim([-30 30])
-title('Car Lateral Velocity')
-
-[~,S] = polyfit(tout(250:end),Vx_NR(250:end),1);
-% plot([5 stoptime],[p(1)*5+p(2) p(1)*stoptime+p(2)])
-plot([5 stoptime],[mean(Vx_NR(501:end)) mean(Vx_NR(501:end))])
-legend('drift v',['mean : ' num2str(mean(Vx_NR(501:end)))])
-
-subplot(3,1,2)
-plot(tout,ster,'m:')
-grid on
-ylabel('[degrees]')
-title('Car Steering Angle')
-
-% wheel outer radius is 30cm
-wheel_rpm = spd/0.3/2/pi;
-
-subplot(3,1,3)
-plot(tout,wheel_rpm,'m:')
-grid on
-ylabel('[rpm]')
-title('Rear Wheel Speed')
-
-MCout(1,n) = steering_step;
-MCout(2,n) = max_speed;
-MCout(3,n) = mean(Vx_NR(501:end));
-MCout(4,n) = S.normr;
-MCout(5,n)  = P;
-MCout(6,n)  = I;
-MCout(7,n)  = D;
-MCout(8,n)  = Pt;
-MCout(9,n)  = It;
-MCout(10,n) = Dt;
-
-
-
-
-
-toPPT(gcf); % By default a new slide is created when adding a figure
-toPPT('setTitle',['P: ' num2str(P) ' ' num2str(Pt) ' I: ' num2str(I) ' ' num2str(It) ' D: ' num2str(D) ' ' num2str(Dt) ' normr: ' num2str(S.normr)]);
-toc
-
-close gcf
+% figure;
+% subplot(3,1,1)
+% hold on
+% plot(tout,Vx_NR,'m:')
+% grid on
+% ylabel('[kph]')
+% ylim([-30 30])
+% title('Car Lateral Velocity')
+% 
+% [~,S] = polyfit(tout(250:end),Vx_NR(250:end),1);
+% % plot([5 stoptime],[p(1)*5+p(2) p(1)*stoptime+p(2)])
+% plot([5 stoptime],[mean(Vx_NR(501:end)) mean(Vx_NR(501:end))])
+% legend('drift v',['mean : ' num2str(mean(Vx_NR(501:end)))])
+% 
+% subplot(3,1,2)
+% plot(tout,ster,'m:')
+% grid on
+% ylabel('[degrees]')
+% title('Car Steering Angle')
+% 
+% % wheel outer radius is 30cm
+% wheel_rpm = spd/0.3/2/pi;
+% 
+% subplot(3,1,3)
+% plot(tout,wheel_rpm,'m:')
+% grid on
+% ylabel('[rpm]')
+% title('Rear Wheel Speed')
+% 
+% MCout(1,n) = steering_step;
+% MCout(2,n) = max_speed;
+% MCout(3,n) = mean(Vx_NR(501:end));
+% MCout(4,n) = S.normr;
+% MCout(5,n)  = P;
+% MCout(6,n)  = I;
+% MCout(7,n)  = D;
+% MCout(8,n)  = Pt;
+% MCout(9,n)  = It;
+% MCout(10,n) = Dt;
+% 
+% 
+% % 
+% 
+% 
+% toPPT(gcf); % By default a new slide is created when adding a figure
+% toPPT('setTitle',['P: ' num2str(P) ' ' num2str(Pt) ' I: ' num2str(I) ' ' num2str(It) ' D: ' num2str(D) ' ' num2str(Dt) ' normr: ' num2str(S.normr)]);
+% toc
+% 
+% close gcf
 
 end
 
 %% if desired, uncomment to save results to a randomly named .mat file
 % uniquename = ['MCout' randseq(3) '.mat'];
 % save(uniquename,'MCout')
-save('MCout_combined_control','MCout','simout')
-% 
+% save('MCout_throttle_control_8','MCout','simout')
+% % 
 % 
 % %% if desired, uncomment to plot results
 % figure
-% scatter3(MCout(1,:),MCout(2,:),MCout(3,:),3,MCout(4,:))
+% scatter3(MCout(1,:),MCout(2,:),abs(MCout(3,:)),3,MCout(4,:))
 % xlabel('steering angle [deg]')
 % ylabel('real wheel speed [rpm]')
-% zlabel('lateral (drift) vel [kph]')
+% zlabel('lateral (drift) speed [|kph|]')
 % colormap(jet)
 % h = colorbar;
 % set(get(h,'title'),'string','norm of residuals \Sigma{\circ}^2 ','Interpreter','tex')
